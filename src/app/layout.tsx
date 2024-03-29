b2246b81aaf3020e78e0aa609842e8c72925718a
dongmin <dongmin11566@gmail.com>
2024-03-29 19:50:39 +0900

    chore : MSW 연동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-undef */
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import React from 'react';
+import { MSWComponent } from './mocks/MSWComponent';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,7 +30,7 @@ export default function RootLayout({
           href="https://i.ibb.co/1GnSm8z/Dream-Vault-Png.png"
           sizes="any"
         />
-        {children}
+        <MSWComponent>{children}</MSWComponent>
       </body>
     </html>
   );
