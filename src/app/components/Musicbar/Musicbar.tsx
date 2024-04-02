60b4739a29fa12ef939f590d63b016846f354755
dongmin <dongmin11566@gmail.com>
2024-04-03 03:13:01 +0900

    feat : 음악 소스 추가

--- src/app/components/Musicbar/Musicbar.tsx
+++ src/app/components/Musicbar/Musicbar.tsx
@@ -40,6 +40,14 @@ export default function MusicBar() {
 
   return (
     <div className="fixed bottom-[1%] items-center w-[83%] h-[7%] rounded-md ml-[16%] px-[2%] py-[0.5%] flex justify-between bg-gradient-to-r from-[#333333] from-20% via-[#7c7a47] via-50%  to-[#333333] to-90% shadow-lg z-40">
+      {/* 음악소스 */}
+      <audio controls preload="auto" className="hidden">
+        <source
+          src="https://s3upload-test-s3.s3.ap-northeast-2.amazonaws.com/Melancholy+Motif.wav"
+          id="audio_player"
+          type="audio/wav"
+        />
+      </audio>
       {/* 재생 컨트롤 버튼 */}
       <div className="flex flex-row py-[0.5%] items-center">
         <ThemeProvider theme={theme}>
