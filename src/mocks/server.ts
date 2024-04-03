a251afe0cad06163f9b23caef8a5bf8b1c2a57e4
j._.jinoo <jinoo0306@naver.com>
2024-04-03 19:21:55 +0900

    refactor : handlers 폴더 추가

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,6 +2,6 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
+import { handlers } from './handlers/handlers';
 
 export const server = setupServer(...handlers);
