0ec6f801fd36ab015d5a512ce2fcfa117f97609e
leedawoon <acebasket42@gmail.com>
2024-04-06 01:36:26 +0900

    refact : 특정 곡 등록하기 핸들러 정의

--- src/mocks/handlers/handlers.ts
+++ src/mocks/handlers/handlers.ts
@@ -113,6 +113,7 @@ export const genreBooleanhandlers = [
 ];
 
 // 마이페이지-내 장르 취향 가져오기
+// 나만의 음악 등록 페이지
 export const takemygenrehandlers = [
   http.get('/api/v1/users/preference', () => {
     return HttpResponse.json({
@@ -182,6 +183,25 @@ export const takemygenrehandlers = [
           },
         ],
       },
+    });
+  }),
+];
+
+// 나만의 음악 등록 페이지 - 특정 곡 등록하기
+export const uploadmymusichandlers = [
+  http.post('/api/v1/tracks', () => {
+    return HttpResponse.json({
+      status: 'success',
+      data: {
+        track_id: 123,
+        title: 'Sunset Boulevard',
+        duration: 120,
+        has_lyrics: true,
+        track_url: 'url/to/track/audio.mp3',
+        track_image: 'url/to/track/image.png',
+        thumbnail_image: 'url/to/track/image.png',
+      },
+      message: 'Track uploaded successfully.',
     });
   }),
 ];
