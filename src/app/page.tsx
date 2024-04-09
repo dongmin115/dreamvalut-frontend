dacaad1e9e1b4fe8749daea304c2d2de7213fa92
leedawoon <acebasket42@gmail.com>
2024-04-09 23:58:24 +0900

    refact : 메인 페이지 page.tsx 원래대로 돌려놓기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@
 // import MusicBar from './components/Musicbar/Musicbar';
 // import NavBar from './components/NavBar/NavigationBar';
 
-// import LogIn from './LogInPage/page';
+import LogIn from './LogInPage/page';
 // import GenrePage from './GenrePage/page';
 // import Page from './main/page';
 // import React from 'react';
-import UploadMyMusic from './MymusicAI/page';
+// import UploadMyMusic from './MymusicAI/page';
 // import Mypage from './mypage/page';
 // App 컴포넌트
 export default function Home() {
@@ -15,11 +15,11 @@ export default function Home() {
     <>
       {/* <NavBar />
       <MusicBar /> */}
-      {/* <LogIn /> */}
+      <LogIn />
       {/* <GenrePage /> */}
       {/* <Mypage /> */}
       {/* <Page /> */}
-      <UploadMyMusic />
+      {/* <UploadMyMusic /> */}
     </>
   );
 }
