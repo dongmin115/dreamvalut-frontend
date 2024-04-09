d01b6015c2aed714f846aa81e0ce128fbeda37d7
j._.jinoo <jinoo0306@naver.com>
2024-04-10 00:50:41 +0900

    refactor : 다른 유저가 만든 플레이리스트 컴포넌트 분리 완료

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -8,11 +8,11 @@ import { ThemeProvider } from '@emotion/react';
 import ForwardIcon from '@mui/icons-material/ArrowForwardIos';
 import BackIcon from '@mui/icons-material/ArrowBackIosNew';
 
-import AlbumCoverUser from '../components/AlbumCover/AlbumCoverUser.tsx';
 import AlbumCoverSystem from '../components/AlbumCover/AlbumCoverSystem.tsx';
 import PopularMusicComponent from './popularMusicComponent.tsx';
 import PopularTagComponent from './popularTagComponent.tsx';
 import GenreMusicComponent from './genreMusicComponent.tsx';
+import OtherPeoplePlayListComponent from './otherPeoplePlayListComponent.tsx';
 
 const theme = createTheme({
   palette: {
@@ -29,18 +29,7 @@ const theme = createTheme({
 
 // 메인 페이지 컴포넌트
 function Page() {
-  const [otherPlaylistPageIndex, setOtherPlaylistPageIndex] = useState(0);
   const [systemPlaylistPageIndex, setSystemPlaylistPageIndex] = useState(0); // 시스템 플레이리스트 페이지 인덱스
-
-  const handleOtherPlaylistPageForwardClick = () => {
-    setOtherPlaylistPageIndex(otherPlaylistPageIndex + 1);
-  };
-
-  const handleOtherPlaylistPageBackwardClick = () => {
-    if (otherPlaylistPageIndex > 0) {
-      setOtherPlaylistPageIndex(otherPlaylistPageIndex - 1);
-    }
-  };
 
   const handleSystemPlaylistPageForwardClick = () => {
     setSystemPlaylistPageIndex(systemPlaylistPageIndex + 1);
@@ -78,46 +67,7 @@ function Page() {
           {/* 다른 유저가 선택한 플레이리스트 */}
           <h1 className="">다른 유저가 선택한 플레이리스트</h1>
           <div className="flex flex-row justify-center items-center w-full h-80 bg-gray-650 rounded-2xl">
-            <IconButton
-              className="w-4"
-              onClick={handleOtherPlaylistPageBackwardClick}
-            >
-              {otherPlaylistPageIndex !== 0 && (
-                <BackIcon color="primary" fontSize="large" />
-              )}
-            </IconButton>
-            <div className="w-11/12 h-full flex flex-row justify-start items-start">
-              <AlbumCoverUser
-                image1="https://i.ibb.co/HgFcPLj/getaguitar.webp"
-                image2="https://i.ibb.co/TbQL5kz/thatthat.jpg"
-                image3="https://i.ibb.co/HV9HB6G/bigbangM.jpg"
-                title="텐션 업!"
-              />
-
-              <AlbumCoverUser
-                image1="https://i.ibb.co/HgFcPLj/getaguitar.webp"
-                image2="https://i.ibb.co/TbQL5kz/thatthat.jpg"
-                image3="https://i.ibb.co/HV9HB6G/bigbangM.jpg"
-                title="낭만 있는 플리"
-              />
-
-              <AlbumCoverUser
-                image1="https://i.ibb.co/HgFcPLj/getaguitar.webp"
-                image2="https://i.ibb.co/TbQL5kz/thatthat.jpg"
-                image3="https://i.ibb.co/HV9HB6G/bigbangM.jpg"
-                title="올드 팝송"
-              />
-
-              <AlbumCoverUser
-                image1="https://i.ibb.co/HgFcPLj/getaguitar.webp"
-                image2="https://i.ibb.co/TbQL5kz/thatthat.jpg"
-                image3="https://i.ibb.co/HV9HB6G/bigbangM.jpg"
-                title="나만 들으려고 저장한 노래"
-              />
-            </div>
-            <IconButton onClick={handleOtherPlaylistPageForwardClick}>
-              <ForwardIcon color="primary" fontSize="large" />
-            </IconButton>
+            <OtherPeoplePlayListComponent />
           </div>
 
           {/* 구독한 플레이리스트 */}
