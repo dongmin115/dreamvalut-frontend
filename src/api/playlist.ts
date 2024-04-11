c88a6b48dfc5612351d36ca7f8a52ceeda27573e
dongmin <dongmin11566@gmail.com>
2024-04-11 15:01:10 +0900

    feat : playlist get요청 api 추가

---
+++ src/api/playlist.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export async function getPlaylist() {
+  try {
+    const response = await axios.get('/api/v1/playlists/{playlist_id}');
+    return response.data;
+  } catch (error) {
+    console.error('오류 발생:', error);
+    throw error;
+  }
+}
+
+export default getPlaylist;
