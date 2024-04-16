38bc6e4270111e38d4617a2d878fd4a51fbadc59
dongmin <dongmin11566@gmail.com>
2024-04-17 01:18:45 +0900

    docs : 주석수정

--- src/api/playlist.ts
+++ src/api/playlist.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 
-// const response = await axios.get(`/api/v1/playlists/${playlistId}`); // 실제 api 연결시 사용
+// const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/genres/list`);
 
 // 특정 플레이리스트 정보 가져오기
 export async function getPlaylist() {
