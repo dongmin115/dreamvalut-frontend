ae85bb2a716a0bbb2f85bb113f5e60a1e916fe3a
leedawoon <acebasket42@gmail.com>
2024-04-20 14:12:40 +0900

    feat : 카카오 소셜 로그인 - 액세스,리프레시 토큰 쿠키에 담기 및 헤더 전송 기능 구현

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,57 +1,43 @@
+import { getCookie } from '@/app/Cookies.tsx';
 import axios from 'axios';
-import { setCookie } from '@/app/Cookies.tsx';
-import jwt from 'jsonwebtoken';
+// import { Cookie } from 'next/font/google';
+// import jwt from 'jsonwebtoken';
 
 const KakaoLogin = () => {
   const handleCallback = async () => {
     // 카카오 로그인 페이지로 리다이렉트합니다.
-    window.location.href = `${process.env.LOGIN_URL}`;
+    window.location.href = 'http://localhost:8080/oauth2/authorization/kakao';
 
     try {
       // Axios를 사용하여 API를 호출하여 JSON 형태의 토큰을 받아옵니다.
-      const response = await axios.get(`${process.env.LOGIN_URL}`);
-      const { accessToken, refreshToken } = response.data;
+      console.log('try시작');
 
       // 액세스 토큰과 리프레시 토큰이 유효한 경우에만 쿠키에 저장합니다.
-      if (accessToken && refreshToken) {
-        // JSON 형태로 토큰 데이터를 만듭니다.
-        const tokenData = {
-          accessToken,
-          refreshToken,
-        };
 
-        // 액세스 토큰과 리프레시 토큰을 jwt로 디코딩하여 JSON 형태로 추출합니다.
-        const decodedAccessToken = jwt.decode(accessToken);
-        const decodedRefreshToken = jwt.decode(refreshToken);
+      // 쿠키에 토큰 데이터를 저장합니다.
+      const a = getCookie('accessToken');
+      const r = getCookie('refreshToken');
 
-        // 쿠키에 토큰 데이터를 저장합니다.
-        setCookie('tokenData', JSON.stringify(tokenData), {
-          path: '/',
-          sameSite: 'strict',
-        });
+      console.log('액세스 토큰과 리프레시 토큰을 쿠키에 저장했습니다.');
+      console.log(a);
+      console.log(r);
 
-        console.log('액세스 토큰과 리프레시 토큰을 쿠키에 저장했습니다.');
-
-        // 쿠키를 헤더에 포함하여 Axios POST 요청을 보냅니다.
-        const headers = {
-          Authorization: `Bearer ${JSON.stringify(decodedAccessToken)}`,
-          'x-refresh-token': JSON.stringify(decodedRefreshToken),
-        };
-
-        try {
-          const res = await axios.post(
-            'http://localhost:8080/refresh',
-            {},
-            {
-              headers,
-            },
-          );
-          console.log('HTTP 요청 성공:', res.data);
-        } catch (error) {
-          console.error('HTTP 요청 실패:', error);
-        }
-      } else {
-        console.error('액세스 토큰 또는 리프레시 토큰이 없습니다.');
+      // 쿠키를 헤더에 포함하여 Axios POST 요청을 보냅니다.
+      const headers = {
+        Authorization: `Bearer ${a}`,
+        'x-refresh-token': r,
+      };
+      try {
+        const response = await axios.post(
+          'http://localhost:8080/refresh',
+          {},
+          {
+            headers,
+          },
+        );
+        console.log('HTTP 요청 성공:', response.data);
+      } catch (error) {
+        console.error('HTTP 요청 실패:', error);
       }
     } catch (error) {
       console.error('API 호출 실패:', error);
