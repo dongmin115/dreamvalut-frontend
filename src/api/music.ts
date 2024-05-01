f90f890ee18108aab247d510929fe17be198bc69
dongmin <dongmin11566@gmail.com>
2024-05-01 18:00:21 +0900

    feat : 음악 좋아요 기능구현

--- src/api/music.ts
+++ src/api/music.ts
@@ -1,16 +1,64 @@
-const getMusic = async (trackId: number) => {
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import axios from 'axios';
+import { getCookie } from '@/app/Cookies';
+
+export const getMusic = async (trackId: number) => {
   try {
-    const response = await fetch(
+    const accessToken = await getCookie('accessToken');
+    const refreshToken = await getCookie('refreshToken');
+    const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}/tracks/${trackId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+          'x-refresh-token': refreshToken,
+        },
+      },
     );
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const responseJson = await response.json();
-    return responseJson;
+    return response.data;
   } catch (error) {
     console.error('오류 발생:', error);
     throw error;
   }
 };
-export default getMusic;
+export const likes = async (trackId: number) => {
+  try {
+    const accessToken = await getCookie('accessToken');
+    const refreshToken = await getCookie('refreshToken');
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/tracks/${trackId}/likes`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+          'x-refresh-token': refreshToken,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error('오류 발생:', error);
+    throw error;
+  }
+};
+
+export const disLikes = async (trackId: number) => {
+  try {
+    const accessToken = await getCookie('accessToken');
+    const refreshToken = await getCookie('refreshToken');
+    const response = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/tracks/${trackId}/disLikes`,
+      {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+          'x-refresh-token': refreshToken,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error('오류 발생:', error);
+    throw error;
+  }
+};
--- src/app/track/[trackId]/page.tsx
+++ src/app/track/[trackId]/page.tsx
@@ -22,8 +22,9 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import { getMyPlaylists, getPlaylist } from '@/api/playlist';
-import getMusic from '@/api/music';
+import { getMusic, disLikes, likes } from '@/api/music';
 import theme from '@/app/styles/theme';
 import { useSharedAudio } from '@/app/components/audio/Audio';
 
@@ -127,10 +128,19 @@ export default function MusicPage(props: any) {
     queryFn: getMyPlaylists,
   });
 
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
   const { data: musicData, isLoading: musicLoading } = useQuery({
     queryKey: ['music'],
     queryFn: () => getMusic(props.params.trackId),
   });
+
+  // useEffect를 사용하여 데이터 로딩이 성공적으로 완료된 후에 상태를 업데이트합니다.
+  useEffect(() => {
+    if (!musicLoading) {
+      setIsLiked(musicData.likes_flag);
+    }
+  }, [musicLoading, musicData]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -141,198 +151,226 @@ export default function MusicPage(props: any) {
           type="audio/wav"
         />
       </audio>
-      <div className="flex h-screen w-screen flex-row justify-around pl-[15%]">
-        {/* 블러배경 */}
-        <img
-          src={musicLoading ? 'loading' : musicData.track_image}
-          alt="1"
-          className="fixed -z-20 h-full w-full blur"
-        />
-        {/* 검은색 레이어 */}
-        <div className="fixed -z-10 h-full w-full bg-black bg-opacity-30" />
-        {/* 음악정보 */}
-        <div className="flex h-full w-[40%] flex-col items-center justify-center space-y-4">
-          <h1 className="text-4xl text-white drop-shadow-lg">
-            {musicLoading ? 'loading' : musicData.title}
-          </h1>
-          <p className="text-[#777777] drop-shadow-md">
-            {musicLoading ? 'loading' : musicData.uploader_name}
-          </p>
-          {/* 프롬프트 + 썸네일 */}
-          <div id="card">
-            <div
-              id="card-back"
-              className="h-96 w-96 rounded-md bg-[#2B2B2B] p-6 drop-shadow-lg"
-            >
-              <p className="mb-4 text-lg font-semibold">음악생성 프롬프트</p>
-              <p className="text-sm">
-                {musicLoading ? 'loading' : musicData.prompt}
-              </p>
-            </div>
-            <img
-              id="card-front"
-              src={musicLoading ? 'loading' : musicData.thumbnail_image}
-              alt="1"
-              className="h-96 w-96 rounded-md drop-shadow-lg"
-            />
-          </div>
-          {/* 재생 컨트롤러 */}
-          <div className="w-96">
-            <div className="mt-6 flex flex-col items-center">
-              <div className="flex w-full flex-row justify-between">
-                <p className="text-sm text-white">{formatTime(currentTime)}</p>
-                <p className="text-sm text-white">
-                  {musicLoading ? 'loading' : formatTime(musicData.duration)}
-                </p>
+      {/* 블러배경 */}
+      {!musicLoading && (
+        <div className="flex h-screen w-screen flex-row justify-around pl-[15%]">
+          <img
+            src={musicData.track_image}
+            alt="1"
+            className="fixed -z-20 h-full w-full blur"
+          />
+          {/* 검은색 레이어 */}
+          <div className="fixed -z-10 h-full w-full bg-black bg-opacity-30" />
+          {/* 음악정보 */}
+          <div className="flex h-full w-[40%] flex-col items-center justify-center space-y-4">
+            <h1 className="text-4xl text-white drop-shadow-lg">
+              {musicData.title}
+            </h1>
+            <p className="text-[#777777] drop-shadow-md">
+              {musicData.uploader_name}
+            </p>
+            {/* 프롬프트 + 썸네일 */}
+            <div id="card">
+              <div
+                id="card-back"
+                className="h-96 w-96 rounded-md bg-[#2B2B2B] p-6 drop-shadow-lg"
+              >
+                <p className="mb-4 text-lg font-semibold">음악생성 프롬프트</p>
+                <p className="text-sm">{musicData.prompt}</p>
               </div>
-              <Slider
-                aria-label="Volume"
-                value={currentTime}
-                onChange={handleSliderChange}
-                onChangeCommitted={handleSliderRelease}
-                onMouseDown={() => setIsDragging(true)} // 슬라이더를 드래그하기 시작하면 상태를 변경합니다.
-                onMouseUp={() => setIsDragging(false)} // 슬라이더에서 손을 떼면 상태를 변경합니다.
-                size="medium"
-                color="secondary"
-                max={musicLoading ? null : musicData.duration}
+              <img
+                id="card-front"
+                src={musicData.thumbnail_image}
+                alt="1"
+                className="h-96 w-96 rounded-md drop-shadow-lg"
               />
             </div>
-            <div className="flex justify-between">
-              <IconButton>
-                <Favorite color="secondary" fontSize="large" />
-              </IconButton>
-              <IconButton>
-                <SkipPreviousIcon color="secondary" fontSize="large" />
-              </IconButton>
-              {isPaused ? (
-                <IconButton
-                  onClick={() => {
-                    playAudio();
-                    setIsPaused(false);
-                  }}
-                  disabled={musicLoading}
-                >
-                  <PlayArrowIcon color="secondary" fontSize="large" />
-                </IconButton>
-              ) : (
-                <IconButton
-                  onClick={() => {
-                    pauseAudio();
-                    setIsPaused(true);
-                  }}
-                >
-                  <PauseIcon color="secondary" fontSize="large" />
-                </IconButton>
-              )}
-              <IconButton>
-                <SkipNextIcon color="secondary" fontSize="large" />
-              </IconButton>
-              <IconButton aria-describedby={id} onClick={handleClick2}>
-                <VolumeUp color="secondary" fontSize="large" />
-              </IconButton>
-              <Popover
-                id={id}
-                open={open2}
-                anchorEl={anchorEl2}
-                onClose={handleClose2}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'center',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'center',
-                }}
-                sx={{
-                  '& .MuiPopover-paper': {
-                    backgroundColor: 'transparent',
-                    border: 'none',
-                    boxShadow: 'none',
-                  },
-                }}
-              >
-                <div className="flex w-[10vw] flex-row items-center space-x-2 rounded-full bg-black bg-opacity-40 px-4 py-2 shadow-md">
-                  <VolumeDown color="secondary" fontSize="medium" />
-                  <Slider
-                    aria-label="Volume"
-                    value={volume}
-                    onChange={handleChange}
-                    size="small"
-                    color="secondary"
-                  />
-                  <VolumeUp color="secondary" fontSize="medium" />
+            {/* 재생 컨트롤러 */}
+            <div className="w-96">
+              <div className="mt-6 flex flex-col items-center">
+                <div className="flex w-full flex-row justify-between">
+                  <p className="text-sm text-white">
+                    {formatTime(currentTime)}
+                  </p>
+                  <p className="text-sm text-white">
+                    {formatTime(musicData.duration)}
+                  </p>
                 </div>
-              </Popover>
+                <Slider
+                  aria-label="Volume"
+                  value={currentTime}
+                  onChange={handleSliderChange}
+                  onChangeCommitted={handleSliderRelease}
+                  onMouseDown={() => setIsDragging(true)} // 슬라이더를 드래그하기 시작하면 상태를 변경합니다.
+                  onMouseUp={() => setIsDragging(false)} // 슬라이더에서 손을 떼면 상태를 변경합니다.
+                  size="medium"
+                  color="secondary"
+                  max={musicData.duration}
+                />
+              </div>
+              <div className="flex justify-between">
+                {isLiked ? (
+                  <IconButton>
+                    <Favorite
+                      color="secondary"
+                      fontSize="large"
+                      onClick={() => {
+                        setIsLiked(false);
+                        disLikes(props.params.trackId).catch(() => {
+                          // API 호출이 실패하면 상태를 되돌립니다
+                          setIsLiked(true);
+                        });
+                      }}
+                    />
+                  </IconButton>
+                ) : (
+                  <IconButton>
+                    <FavoriteBorder
+                      color="secondary"
+                      fontSize="large"
+                      onClick={() => {
+                        setIsLiked(true);
+                        likes(props.params.trackId).catch(() => {
+                          // API 호출이 실패하면 상태를 되돌립니다
+                          setIsLiked(false);
+                        });
+                      }}
+                    />
+                  </IconButton>
+                )}
+                <IconButton>
+                  <SkipPreviousIcon color="secondary" fontSize="large" />
+                </IconButton>
+                {isPaused ? (
+                  <IconButton
+                    onClick={() => {
+                      playAudio();
+                      setIsPaused(false);
+                    }}
+                    disabled={musicLoading}
+                  >
+                    <PlayArrowIcon color="secondary" fontSize="large" />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      pauseAudio();
+                      setIsPaused(true);
+                    }}
+                  >
+                    <PauseIcon color="secondary" fontSize="large" />
+                  </IconButton>
+                )}
+                <IconButton>
+                  <SkipNextIcon color="secondary" fontSize="large" />
+                </IconButton>
+                <IconButton aria-describedby={id} onClick={handleClick2}>
+                  <VolumeUp color="secondary" fontSize="large" />
+                </IconButton>
+                <Popover
+                  id={id}
+                  open={open2}
+                  anchorEl={anchorEl2}
+                  onClose={handleClose2}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  sx={{
+                    '& .MuiPopover-paper': {
+                      backgroundColor: 'transparent',
+                      border: 'none',
+                      boxShadow: 'none',
+                    },
+                  }}
+                >
+                  <div className="flex w-[10vw] flex-row items-center space-x-2 rounded-full bg-black bg-opacity-40 px-4 py-2 shadow-md">
+                    <VolumeDown color="secondary" fontSize="medium" />
+                    <Slider
+                      aria-label="Volume"
+                      value={volume}
+                      onChange={handleChange}
+                      size="small"
+                      color="secondary"
+                    />
+                    <VolumeUp color="secondary" fontSize="medium" />
+                  </div>
+                </Popover>
+              </div>
             </div>
           </div>
-        </div>
-        {/* 재생목록 */}
-        <div className="flex h-full w-[30%] flex-col items-center justify-center space-y-4">
-          <div className="flex h-fit w-full flex-row items-center justify-between">
-            <h1 className="m-0 h-fit text-4xl text-white drop-shadow-lg">
-              Playlist
-            </h1>
-            <Button
-              color="secondary"
-              id="basic-button"
-              aria-controls={open ? 'basic-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-            >
-              재생목록 선택
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            >
-              {listLoading
-                ? 'loading'
-                : listData.data.playlists.map((e: any) => (
-                    <MenuItem
-                      key={e.playlist_id}
-                      onClick={() => {
-                        handleClose();
-                        // setSelectedPlaylist(e.playlist_id);
-                      }}
-                    >
-                      {e.playlist_name}
-                    </MenuItem>
-                  ))}
-            </Menu>
+          {/* 재생목록 */}
+          <div className="flex h-full w-[30%] flex-col items-center justify-center space-y-4">
+            <div className="flex h-fit w-full flex-row items-center justify-between">
+              <h1 className="m-0 h-fit text-4xl text-white drop-shadow-lg">
+                Playlist
+              </h1>
+              <Button
+                color="secondary"
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              >
+                재생목록 선택
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}
+              >
+                {listLoading
+                  ? 'loading'
+                  : listData.data.playlists.map((e: any) => (
+                      <MenuItem
+                        key={e.playlist_id}
+                        onClick={() => {
+                          handleClose();
+                          // setSelectedPlaylist(e.playlist_id);
+                        }}
+                      >
+                        {e.playlist_name}
+                      </MenuItem>
+                    ))}
+              </Menu>
+            </div>
+            <Divider
+              variant="fullWidth"
+              orientation="horizontal"
+              flexItem
+              className="w-full bg-white drop-shadow-xl"
+            />
+            {/* 재생목록 리스트 */}
+            {isLoading
+              ? 'loading'
+              : data.tracks.content.map((track: any) => (
+                  <li
+                    key={track.id}
+                    className="flex w-full flex-row space-x-4 self-start p-2 hover:rounded-md hover:bg-[#040404] hover:bg-opacity-30"
+                  >
+                    <img
+                      src={track.thumbnail_image}
+                      alt="음악 커버"
+                      className="h-16 w-16 rounded-md drop-shadow-lg"
+                    />
+                    <div className="flex flex-col items-center justify-center">
+                      <p className="text-lg text-white">{track.title}</p>
+                      <p className="text-[#777777]">{track.uploader_name}</p>
+                    </div>
+                  </li>
+                ))}
           </div>
-          <Divider
-            variant="fullWidth"
-            orientation="horizontal"
-            flexItem
-            className="w-full bg-white drop-shadow-xl"
-          />
-          {/* 재생목록 리스트 */}
-          {isLoading
-            ? 'loading'
-            : data.tracks.content.map((track: any) => (
-                <li
-                  key={track.id}
-                  className="flex w-full flex-row space-x-4 self-start p-2 hover:rounded-md hover:bg-[#040404] hover:bg-opacity-30"
-                >
-                  <img
-                    src={track.thumbnail_image}
-                    alt="음악 커버"
-                    className="h-16 w-16 rounded-md drop-shadow-lg"
-                  />
-                  <div className="flex flex-col items-center justify-center">
-                    <p className="text-lg text-white">{track.title}</p>
-                    <p className="text-[#777777]">{track.uploader_name}</p>
-                  </div>
-                </li>
-              ))}
         </div>
-      </div>
+      )}
     </ThemeProvider>
   );
 }
