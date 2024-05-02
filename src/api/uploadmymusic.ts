c3167615037f96ea2007cdcf4b67b73bbff8be80
leedawoon <acebasket42@gmail.com>
2024-05-02 13:11:05 +0900

    refact : 불필요한 주석 삭제

--- src/api/uploadmymusic.ts
+++ src/api/uploadmymusic.ts
@@ -1,46 +1,4 @@
-// /* eslint-disable consistent-return */
-// // import { Genre } from '@/types/genre.ts';
-// import axios from 'axios';
-
-// // 특정 곡 등록하기
-// const uploadMymusic = async (
-//   title: string,
-//   prompt: string,
-//   hasLyrics: boolean,
-//   tags: string[],
-//   genreId: number,
-//   trackImage: File,
-//   trackAudio: File,
-// ) => {
-//   try {
-//     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/tracks`, {
-//       track_info: {
-//         title,
-//         prompt,
-//         has_lyrics: hasLyrics,
-//         tags,
-//         genre_id: genreId,
-//       },
-//       track_image: trackImage,
-//       track_audio: trackAudio,
-//     }, {
-//       headers: {
-//         'Content-Type': 'multipart/form-data',
-//       },
-//     });
-
-//     if (response.data) {
-//       return response.data.data; // 장르 데이터 반환
-//     }
-//   } catch (error) {
-//     console.error('오류 발생', error);
-//   }
-// };
-
-// export default uploadMymusic;
-
 /* eslint-disable consistent-return */
-// import { Genre } from '@/types/genre.ts';
 import axios from 'axios';
 
 // 특정 곡 등록하기
