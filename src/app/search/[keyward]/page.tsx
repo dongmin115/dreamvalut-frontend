3dd2cfbab22b7fbda220451db4a1cd753c8c4646
dongmin <dongmin11566@gmail.com>
2024-05-08 16:07:42 +0900

    feat : 검색 결과 갯수 표시

--- src/app/search/[keyward]/page.tsx
+++ src/app/search/[keyward]/page.tsx
@@ -13,6 +13,7 @@ import Divider from '@mui/material/Divider';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { searchResult } from '@/types/search';
+import { getCookie } from '@/app/Cookies';
 
 const theme = createTheme({
   palette: {
@@ -31,8 +32,22 @@ export default function SearchPage(props: any) {
   const { data, isLoading } = useQuery({
     queryKey: ['search'],
     queryFn: async () => {
-      const response = await axios.get('/api/v1/search');
-      return response.data.data.results;
+      const accessToken = await getCookie('accessToken');
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/search`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+          params: {
+            query: props.params.keyward,
+            page: 0,
+          },
+        },
+      );
+      console.log(response.data);
+      return response.data;
     },
   });
 
@@ -40,49 +55,49 @@ export default function SearchPage(props: any) {
     <ThemeProvider theme={theme}>
       <div className="h-fit min-h-screen">
         {/* NavBar 제외영역 */}
-        <div className="pl-[15%] h-fit w-full">
-          <div className="flex p-[3%] flex-col gap-8 w-full">
-            <p className="text-xl w-fit">
-              <em>{decodeURIComponent(props.params.keyward)}</em>에 대한 검색
-              결과입니다.
+        <div className="h-fit w-full pl-[15%]">
+          <div className="flex w-full flex-col gap-8 p-[3%]">
+            <p className="w-fit text-xl">
+              <em>{decodeURIComponent(props.params.keyward)}</em> 에 대한 검색
+              결과 {data.total_elements}건 입니다.
             </p>
-            <div className="flex flex-col bg-[#353535] h-fit rounded-xl w-full p-[2%] gap-4">
-              <div className="flex flex-row justify-around items-center h-fit">
-                <p className="text-xl w-[60%] text-center">곡정보</p>
-                <p className="text-lg w-[10%] text-center">제작자</p>
-                <p className="text-lg w-[10%] text-center">좋아요</p>
-                <p className="text-lg w-[10%] text-center">재생</p>
+            <div className="flex h-fit w-full flex-col gap-4 rounded-xl bg-[#353535] p-[2%]">
+              <div className="flex h-fit flex-row items-center justify-around">
+                <p className="w-[60%] text-center text-xl">곡정보</p>
+                <p className="w-[10%] text-center text-lg">제작자</p>
+                <p className="w-[10%] text-center text-lg">좋아요</p>
+                <p className="w-[10%] text-center text-lg">재생</p>
               </div>
               <Divider />
               {/* 검색 결과 리스트 */}
-              <ul className="flex flex-col gap-8 min-h-[70vh] h-fit">
+              <ul className="flex h-fit min-h-[70vh] flex-col gap-8">
                 {isLoading ? (
-                  <div className="h-full w-full text-center my-auto text-2xl">
+                  <div className="my-auto h-full w-full text-center text-2xl">
                     {decodeURIComponent(props.params.keyward)}에 대한 검색 결과
                     가져오는중...
                   </div>
                 ) : (
-                  data.map((e: searchResult, i: number) => (
+                  data.content.map((e: searchResult, i: number) => (
                     <li
                       key={i}
-                      className="flex flex-row justify-around items-center h-fit"
+                      className="flex h-fit flex-row items-center justify-around"
                     >
                       {/* 앨범 커버, 곡 이름 + 태그, 프롬프트 내용 flexbox */}
-                      <div className="flex flex-row justify-between items-center w-[60%] gap-8">
+                      <div className="flex w-[60%] flex-row items-center justify-between gap-8">
                         {/* 앨범 커버, 곡 이름 flexbox */}
-                        <div className="flex flex-col justify-center items-center w-fit gap-4">
+                        <div className="flex w-fit flex-col items-center justify-center gap-4">
                           <img
                             src={e.thumbnail_image}
                             alt="cover"
                             className="size-28 rounded-sm"
                           />
                           <p
-                            className="xs:text-xs md:text-xs lg:text-xs xl:text-sm w-fit text-center"
+                            className="xs:text-xs w-fit text-center md:text-xs lg:text-xs xl:text-sm"
                             dangerouslySetInnerHTML={{ __html: e.title }}
                           />
                         </div>
                         {/* 태그, 프롬프트 내용 flexbox */}
-                        <div className="flex flex-col justify-center items-center w-[80%] gap-2">
+                        <div className="flex w-[80%] flex-col items-center justify-center gap-2">
                           {/* 태그 flexbox */}
                           <div className="flex flex-row gap-2 self-start">
                             {/* 태그 */}
@@ -97,22 +112,22 @@ export default function SearchPage(props: any) {
                           </div>
                           {/* 프롬프트 내용 */}
                           <p
-                            className="text-md items-center w-full"
+                            className="text-md w-full items-center"
                             dangerouslySetInnerHTML={{ __html: e.prompt }}
                           />
                         </div>
                       </div>
                       {/* 제작자 */}
                       <p
-                        className="text-lg w-[10%] text-center text-[#777777]"
+                        className="w-[10%] text-center text-lg text-[#777777]"
                         dangerouslySetInnerHTML={{ __html: e.uploader_name }}
                       />
                       {/* 좋아요 */}
-                      <div className="flex flex-row justify-center gap-2 items-center w-[10%]">
+                      <div className="flex w-[10%] flex-row items-center justify-center gap-2">
                         <IconButton>
                           <FavoriteIcon color="primary" fontSize="medium" />
                         </IconButton>
-                        <p className="text-lg w-fit text-center">{e.likes}</p>
+                        <p className="w-fit text-center text-lg">{e.likes}</p>
                       </div>
                       {/* 재생 */}
                       <div className="w-[10%] text-center">
