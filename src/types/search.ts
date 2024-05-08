eebc6206f6f25aebf994ca44e3aa52557cf11510
dongmin <dongmin11566@gmail.com>
2024-05-08 16:43:26 +0900

    feat : 검색 결과 타입 수정

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,10 +1,51 @@
-export interface searchResult {
-  track_id: number;
+/* eslint-disable semi */
+export interface Genre {
+  genre_id: number;
+  genre_name: string;
+}
+
+export interface Tag {
+  tag_id: number;
+  tag_name: string;
+}
+
+export interface TrackInfo {
+  id: number;
   title: string;
   uploader_name: string;
-  genre_name: string; // 이 이분 강조됨
-  thumbnail_image: string;
-  prompt: string; // 이 이분 강조됨
-  tags: string;
+  prompt: string;
+  track_genre: Genre;
+  track_tags: Tag[];
   likes: number;
+  likes_flag: boolean;
+  thumbnail_image: string;
+}
+
+export interface SortInfo {
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface Pageable {
+  page_number: number;
+  page_size: number;
+  sort: SortInfo;
+  offset: number;
+  unpaged: boolean;
+  paged: boolean;
+}
+
+export interface searchResult {
+  content: TrackInfo[];
+  pageable: Pageable;
+  total_elements: number;
+  total_pages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: SortInfo;
+  first: boolean;
+  number_of_elements: number;
+  empty: boolean;
 }
