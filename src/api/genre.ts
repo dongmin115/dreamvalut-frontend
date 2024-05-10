f8d04d483e1ac1c4773c4956b098698a119dffd5
leedawoon <acebasket42@gmail.com>
2024-05-10 15:36:21 +0900

    feat : api 요청 부분 푸시

--- src/api/genre.ts
+++ src/api/genre.ts
@@ -5,10 +5,10 @@
 import { getCookie } from '@/app/Cookies.tsx';
 import axios from 'axios';
 
+const accessToken = getCookie('accessToken');
 // 모든 장르 데이터 가져오기
 const fetchGenres = async () => {
   try {
-    const accessToken = await getCookie('accessToken');
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}/genres/list`,
       {
@@ -31,8 +31,12 @@ const fetchGenres = async () => {
 // 내 장르 취향 가져오기
 const EditfetchGenres = async () => {
   try {
-    // 실제 서버 대신 msw의 모의 서버로 요청을 보냅니다.
-    const response = await axios.get('/api/v1/users/preference');
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users/preference`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     const genresData = response.data.data.genres;
     return genresData; // 장르 데이터 반환
   } catch (error) {
