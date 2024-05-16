e05e1b9cb74448d1b713e515e841364c37eaaa48
dongmin <dongmin11566@gmail.com>
2024-05-16 17:26:55 +0900

    feat : 무한 스크롤 공통컴포넌트 생성

---
+++ src/app/components/InfiniteScroll.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable consistent-return */
+/* eslint-disable comma-spacing */
+/* eslint-disable object-curly-newline */
+/* eslint-disable operator-linebreak */
+import React, { useEffect, useRef } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const InfiniteScroll = ({
+  queryKey,
+  queryFn,
+  renderItem,
+  getNextPageParam,
+  dataPath,
+}) => {
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey,
+      queryFn,
+      getNextPageParam,
+      initialPageParam: 0,
+    });
+
+  const loadMoreRef = useRef(null);
+
+  useEffect(() => {
+    if (!hasNextPage) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      {
+        root: null, // 기본적으로 브라우저 뷰포트를 root로 사용
+        rootMargin: '0px',
+        threshold: 0.1, // 타겟 요소가 10% 보이면 콜백 실행
+      },
+    );
+    if (loadMoreRef.current) observer.observe(loadMoreRef.current);
+
+    return () => {
+      if (loadMoreRef.current) observer.disconnect();
+    };
+  }, [hasNextPage, fetchNextPage]);
+
+  return (
+    <div className="w-full">
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error loading data</div>}
+      {data?.pages.map((page, pageIndex) => (
+        <React.Fragment key={pageIndex}>
+          {dataPath(page).map((item, index: number) => renderItem(item, index))}
+        </React.Fragment>
+      ))}
+      {isLoading && <div>Loading more items...</div>}
+      {!hasNextPage && <div>No more items to load</div>}
+      <div ref={loadMoreRef} />
+    </div>
+  );
+};
+
+export default InfiniteScroll;
