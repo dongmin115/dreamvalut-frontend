b716f755785f5897f7320738609054ac76395494
leedawoon <acebasket42@gmail.com>
2024-05-24 16:55:47 +0900

    refact: Image -> img로 변경, design: 로그인 버튼 안에 이미지 딱 맞게끔 디자인 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,54 +23,52 @@ const LogIn = () => (
         Welcome To <span className="z-10 text-violet-600">DreamVault.</span>
       </p>
       <p className="z-10 mb-16 text-6xl text-white">당신의 꿈을 열어보세요.</p>
-      <div className="LogInBtnsGoogle z-10 mb-4 flex h-[5%] w-1/6 items-center justify-start rounded-md">
-        <div className="flex h-10 w-10 justify-start">
-          <div className="relative h-full w-full">
-            <Image
-              src="https://i.ibb.co/09htLD8/web-neutral-sq-na-2x.png"
-              alt="googleimage"
-              fill={true} // 부모 컨테이너를 채우도록 설정
-              objectFit="cover" // 이미지가 부모 컨테이너에 맞게 조정됨
-            />
-          </div>
+      <div className="LogInBtnsGoogle z-10 mb-4 flex h-10 w-1/6 items-center justify-start rounded-md">
+        <div className="flex h-10 w-10 items-center justify-center">
+          <img
+            src="https://i.ibb.co/09htLD8/web-neutral-sq-na-2x.png"
+            alt="googleimage"
+            className="h-full w-full object-cover"
+          />
         </div>
         <button
           onClick={GoogleLogin}
-          className="ml-[25%] text-base font-medium"
+          className="ml-2 h-10 flex-1 text-base font-medium"
         >
           구글 로그인
         </button>
       </div>
-      <div className="LogInBtnsKakao z-10 mb-4 flex h-[5%] w-1/6 items-center justify-start rounded-md">
-        <div className="flex h-10 w-10 justify-start">
-          <div className="relative h-full w-full">
-            <Image
-              src="https://i.ibb.co/r6y52R7/kakaotalk-sharing-btn-small-ov.png"
-              alt="kakaoimage"
-              fill={true}
-              objectFit="cover"
-            />
-          </div>
+      <div className="LogInBtnsKakao z-10 mb-4 flex h-10 w-1/6 items-center justify-start rounded-md">
+        <div className="flex h-10 w-10 items-center justify-center">
+          <img
+            src="https://i.ibb.co/r6y52R7/kakaotalk-sharing-btn-small-ov.png"
+            alt="kakaoimage"
+            className="h-full w-full object-cover"
+          />
         </div>
-        <button onClick={KakaoLogin} className="ml-[25%] text-base font-medium">
+        <button
+          onClick={KakaoLogin}
+          className="ml-2 h-10 flex-1 text-base font-medium"
+        >
           카카오 로그인
         </button>
       </div>
-      <div className="LogInBtnsNaver z-10 mb-4 flex h-[5%] w-1/6 items-center justify-start rounded-md">
-        <div className="flex h-10 w-10 justify-start">
-          <div className="relative h-full w-full">
-            <Image
-              src="https://i.ibb.co/wYf09Y3/btn-G.png"
-              alt="naverimage"
-              fill={true}
-              objectFit="cover"
-            />
-          </div>
+      <div className="LogInBtnsNaver z-10 mb-4 flex h-10 w-1/6 items-center justify-start rounded-md">
+        <div className="flex h-10 w-10 items-center justify-center">
+          <img
+            src="https://i.ibb.co/wYf09Y3/btn-G.png"
+            alt="naverimage"
+            className="h-full w-full object-cover"
+          />
         </div>
-        <button onClick={NaverLogin} className="ml-[25%] text-base font-medium">
+        <button
+          onClick={NaverLogin}
+          className="ml-2 h-10 flex-1 text-base font-medium"
+        >
           네이버 로그인
         </button>
       </div>
+
       <div className="z-1">
         <div className="wave -one"></div>
         <div className="wave -two"></div>
