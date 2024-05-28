33c8036b137472e8f6ec6e780821487e544ace8f
leedawoon <acebasket42@gmail.com>
2024-05-29 04:06:42 +0900

    style : 장르 페이지 반응형 적용 및 버튼 디자인 수정

--- src/app/genre_select/page.tsx
+++ src/app/genre_select/page.tsx
@@ -2,7 +2,6 @@
 
 import { Suspense, useEffect, useState } from 'react';
 import { ToggleButton, ToggleButtonGroup } from '@mui/material';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { ThemeProvider } from '@mui/material/styles';
 import { useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
@@ -39,71 +38,85 @@ const GenrePageContent = () => {
   return (
     <ThemeProvider theme={theme}>
       <ToggleButtonGroup orientation="vertical" value={selectedGenres}>
-        <div>
-          <div className="flex h-screen w-screen items-center justify-center">
-            <div className="z-10 grid h-screen w-[30%] grid-cols-3 grid-rows-5 gap-4 p-[1%]">
-              {genres.map((genre) => (
-                <div key={genre.genre_id} className="w-full">
-                  <ToggleButton
-                    value={genre.genre_id}
-                    onClick={() => {
-                      handleGenreToggle(genre.genre_id);
-                    }}
-                    className={`fade-in-box hover-bg-opacity flex h-full w-full flex-col text-center ${selectedGenres.includes(genre.genre_id) ? '#341672' : ''}`}
-                    style={{
-                      border: selectedGenres.includes(genre.genre_id)
-                        ? '1px solid #8b5cf6'
-                        : '1px solid #8b5cf6',
-                      backgroundColor: selectedGenres.includes(genre.genre_id)
-                        ? '#2e1065'
-                        : '#1a1a1a',
-                      borderRadius: '15%',
-                    }}
-                  >
-                    <div className="relative flex h-4/5 w-4/5">
-                      <Image
-                        src={genre.genre_image}
-                        alt="genre-thumbnail"
-                        className="rounded-full"
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </div>
-                    <p className="mt-[15%] text-sm text-white">
-                      {genre.genre_name}
-                    </p>
-                  </ToggleButton>
-                </div>
-              ))}
-            </div>
+        <div className="flex h-screen w-screen items-center justify-center overflow-y-auto">
+          <div className="flex h-screen max-w-[30rem] flex-wrap items-center justify-center">
+            {genres.map((genre) => (
+              <div
+                key={genre.genre_id}
+                className="z-10 flex h-1/5 max-h-[10rem] w-1/3 p-2"
+              >
+                <ToggleButton
+                  value={genre.genre_id}
+                  onClick={() => {
+                    handleGenreToggle(genre.genre_id);
+                  }}
+                  className={`fade-in-box hover-bg-opacity flex h-full w-full flex-col text-center ${selectedGenres.includes(genre.genre_id) ? '#341672' : ''}`}
+                  style={{
+                    border: selectedGenres.includes(genre.genre_id)
+                      ? '1px solid #280E52'
+                      : '1px solid #4E4B54',
+                    backgroundColor: selectedGenres.includes(genre.genre_id)
+                      ? '#280E52'
+                      : '#1a1a1a',
+                    borderRadius: '15%',
+                  }}
+                >
+                  <div className="relative mx-auto flex h-4/5 w-4/5">
+                    <Image
+                      src={genre.genre_image}
+                      alt="genre-thumbnail"
+                      className="rounded-full"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <p className="mt-[15%] text-xs text-white sm:text-sm">
+                    {genre.genre_name}
+                  </p>
+                </ToggleButton>
+              </div>
+            ))}
           </div>
-          <div className="fade-in-box2 absolute left-0 top-[45%] z-10 text-violet-900 opacity-[100%]">
-            <p className="text-8xl">Genre.</p>
-          </div>
-          <div className="fade-in-box2 absolute bottom-[43%] left-[1%] z-0 h-[1%] w-[98%] rounded-md bg-violet-950 opacity-[100%]"></div>
         </div>
+
+        <div className="fade-in-box2 absolute left-0 top-[45%] z-10 text-violet-900 opacity-[100%]">
+          <p className="text-7xl opacity-45 sm:text-8xl sm:opacity-45 md:opacity-45 lg:opacity-100 ">
+            Genre.
+          </p>
+        </div>
+        <div className="fade-in-box2 absolute bottom-[43%] left-[1%] z-0 h-[1%] w-[98%] rounded-md bg-violet-950 opacity-45 sm:opacity-45 md:opacity-45 lg:opacity-100"></div>
       </ToggleButtonGroup>
       {/* 다음 페이지로 이동하는 버튼 */}
-      <div>
-        <button
-          className="genreBtns fixed bottom-0 right-0 h-[12%] w-[8%]"
-          onClick={async () => {
-            await postGenreTaste(selectedGenres).then(() => {
-              router.push('/main');
-            });
-          }}
-        >
-          <ArrowForwardIosIcon color="primary" fontSize="large" />
-        </button>
-      </div>
+      <button
+        className="svg-wrapper fixed bottom-0 right-0"
+        onClick={async () => {
+          await postGenreTaste(selectedGenres).then(() => {
+            router.push('/main');
+          });
+        }}
+      >
+        <svg className="h-full w-full" xmlns="http://www.w3.org/2000/svg">
+          <rect
+            id="shape"
+            className="h-full w-full fill-current text-indigo-600"
+          />
+          <text
+            x="50%"
+            y="50%"
+            textAnchor="middle"
+            dominantBaseline="middle"
+            className="fill-white"
+          >
+            NEXT
+          </text>
+        </svg>
+      </button>
     </ThemeProvider>
   );
 };
 
-// options: ClientSearchParamSetterOptions
 const GenrePage = () => (
   <Suspense fallback={<div>Loading...</div>}>
-    {/* <GenrePageContent {...options} /> */}
     <GenrePageContent />
   </Suspense>
 );
