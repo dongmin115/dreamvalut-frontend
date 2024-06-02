361150a01d5d8fefe3eafa3b205fc53246235d71
leedawoon <acebasket42@gmail.com>
2024-06-02 15:57:39 +0900

    test : 주석문 삭제

--- src/api/logout.ts
+++ src/api/logout.ts
@@ -1,12 +1,10 @@
 import { getCookie, removeCookie } from '@/app/Cookies.tsx';
 import axios from 'axios';
 
-// 로그아웃 함수를 정의합니다.
 const logout = () => {
   const accessToken = getCookie('accessToken');
   const refreshToken = getCookie('refreshToken');
 
-  // 토큰이 없으면 에러
   if (!accessToken || !refreshToken) {
     throw new Error('No tokens found in cookies');
   }
@@ -28,10 +26,6 @@ const logout = () => {
       return response.data;
     })
     .catch((error) => {
-      // 에러가 발생하면 에러 메시지를 콘솔에 출력합니다.
-      console.error('Error during logout:', error);
-
-      // 에러 객체를 던져 호출자가 처리할 수 있도록 합니다.
       throw error;
     });
 };
